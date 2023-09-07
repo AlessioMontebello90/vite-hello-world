4039648a5f0d85e31ae0b359849125348850e635
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-07 17:52:26 +0200

    add img

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
+import GalleryShow from "./components/GalleryShow.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <HelloWorld></HelloWorld>
+  <GalleryShow></GalleryShow>
 </template>
 
 <style>
 h1 {
   color: green;
+}
+
+img {
+  width: 100%;
 }
 </style>
---
+++ src/components/GalleryShow.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      img: "./img/img.jpeg",
+    };
+  },
+};
+</script>
+
+<template>
+  <img :src="img" />
+</template>
+
+<style></style>
